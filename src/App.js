0a780a44c872b3001914af0ff3b347a2cfa0cbc9
ZubairAmin695 <zubairamin695@gmail.com>
2023-10-11 15:13:44 +0500

    filters are added and are working properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import img from "./Image.jpeg";
 
 function App() {
+  const [brightness, setBrightness] = useState({
+    name: "Brightness",
+    property: "brightness",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  });
+
+  const [contrast, setContrast] = useState({
+    name: "Contrast",
+    property: "contrast",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  });
+
+  const [saturation, setSaturation] = useState({
+    name: "Saturation",
+    property: "saturate",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  });
+
+  const [grayscale, setGreyscale] = useState({
+    name: "Grayscale",
+    property: "grayscale",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  });
+
+  const [sepia, setSepia] = useState({
+    name: "Sepia",
+    property: "sepia",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  });
+
+  const [hueRotate, setHueRotate] = useState({
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: "deg",
+  });
+  const [blur, setBlur] = useState({
+    name: "Blur",
+    property: "blur",
+    value: 0,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: "px",
+  });
+
   return (
-    
- <>hello word
- </>
+    <>
+      <div className="wrapper">
+        <div>
+          <div className="image-wrapper">
+            <div className="image">
+              <img
+                src={img}
+                alt=""
+                style={{
+                  filter: `${brightness.property}(${brightness.value}${brightness.unit}) ${contrast.property}(${contrast.value}${contrast.unit}) ${saturation.property}(${saturation.value}${saturation.unit}) ${sepia.property}(${sepia.value}${sepia.unit}) ${hueRotate.property}(${hueRotate.value}${hueRotate.unit}) ${blur.property}(${blur.value}${blur.unit})`,
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="options">
+            <div className="mode">
+              <span>Brightness</span>
+              <input
+                type="range"
+                max={brightness.range.max}
+                min={brightness.range.min}
+                onChange={(e) => {
+                  setBrightness({ ...brightness, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+
+            <div className="mode">
+              <span>Contrast</span>
+              <input
+                type="range"
+                max={contrast.range.max}
+                min={contrast.range.min}
+                onChange={(e) => {
+                  setContrast({ ...contrast, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+
+            <div className="mode">
+              <span>Saturation</span>
+              <input
+                type="range"
+                max={saturation.range.max}
+                min={saturation.range.min}
+                onChange={(e) => {
+                  setSaturation({ ...saturation, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+
+            <div className="mode">
+              <span>Sepia</span>
+              <input
+                type="range"
+                max={sepia.range.max}
+                min={sepia.range.min}
+                onChange={(e) => {
+                  setSepia({ ...sepia, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+
+            <div className="mode">
+              <span>Hue Rotate</span>
+              <input
+                type="range"
+                max={hueRotate.range.max}
+                min={hueRotate.range.min}
+                onChange={(e) => {
+                  setHueRotate({ ...hueRotate, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+
+            <div className="mode">
+              <span>Blur</span>
+              <input
+                type="range"
+                max={blur.range.max}
+                min={blur.range.min}
+                onChange={(e) => {
+                  setBlur({ ...blur, value: `${e.target.value}` });
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
